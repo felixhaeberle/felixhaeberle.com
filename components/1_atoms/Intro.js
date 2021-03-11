eff799443fe2737fa69001426369af57a46f685b
Felix Haeberle <kontakt@felixhaeberle.de>
2021-03-11 23:16:27 +0100

    update Intro responsive

--- components/1_atoms/Intro.js
+++ components/1_atoms/Intro.js
@@ -3,8 +3,8 @@ import Text from './Text'
 
 export default function Intro(){
   return (
-    <r-grid columns="6" columns-s="2" columns-xs="1">
-      <r-cell span="row" flow-cols="3" flow-cols-s="2" flow-cols-xs="1">
+    <r-grid columns="6">
+      <r-cell span="row" flow-cols="3" flow-cols-m="2" flow-cols-s="1">
         <Text.Intro>My career as a designer started in the 1990s with an online magazine I created with a few friends. I started helping others with design and websites to learn more. It eventually led me to my first job as a designer, at Lear Corporation in 1999.</Text.Intro>
         <Text.Intro>My career as a designer started in the 1990s with an online magazine I created with a few friends. I started helping others with design and websites to learn more. It eventually led me to my first job as a designer, at Lear Corporation in 1999.</Text.Intro>
         <Text.Intro>My career as a designer started in the 1990s with an online magazine I created with a few friends. I started helping others with design and websites to learn more. It eventually led me to my first job as a designer, at Lear Corporation in 1999.</Text.Intro>
