d7dea3682d43e683cc3d1d4bc8ba431a153e30ef
Felix Haeberle <kontakt@felixhaeberle.de>
2021-04-24 23:56:27 +0200

    update syntax component

--- components/1_atoms/Syntax.js
+++ components/1_atoms/Syntax.js
@@ -1,10 +1,9 @@
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import Text from './Text'
 import adjustedSyntaxStyles from '../0_helpers/syntaxStyles';
 import dynamic from "next/dynamic";
 import style from 'react-syntax-highlighter/dist/cjs/styles/prism/dracula';
 import styled from 'styled-components'
-import { useEffect } from "react";
 
 const SyntaxHighlighterItem = styled.div`
   display: flex;
@@ -36,11 +35,6 @@ SyntaxHighlighterItem.SyntaxWrapper = styled.pre`
 `
 
 export default function Syntax({ langCode, code }) {
-  // useEffect(() => {
-  //   const LoadLanguage = dynamic(
-  //     () => import('react-syntax-highlighter/dist/cjs/languages/prism/' + langCode)
-  //     .then((langCode) => SyntaxHighlighter.registerLanguage(String(langCode), langCode)), {ssr: false})
-  // }, [])
 
   // Convert lang string for visual output
   const langCodeUppercase = String(langCode).toUpperCase();
